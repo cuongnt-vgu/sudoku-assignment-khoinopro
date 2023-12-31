9edc71bcdec3cdc4ceb75ffbcde6bc6e110eb631
khoinopro <135790463+khoinopro@users.noreply.github.com>
2023-12-31 15:15:10 +0700

    Create hidden_pairs.h

--- src/hidden_pairs.h
+++ src/hidden_pairs.h
@@ -3,3 +3,10 @@
 #include "sudoku.h"
 
 int hidden_pairs(SudokuBoard *p_board);
+
+typedef struct {
+    Cell *p_cell_1;  // Pointer to the cell where the hidden single is found
+    Cell *p_cell_2; 
+    int value1;           // The value of the hidden single
+    int value2;
+} HiddenPair;
